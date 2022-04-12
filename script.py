f93d3c0b9f6a8983345785ec506fb85a79290855
msr8 <live.shashwat@gmail.com>
2022-04-12 22:19:55 +0530

    rewrite

---
+++ script.py
@@ -0,0 +1,287 @@
+HELP_TEXT = 'You are a crewmate and your color is Yellow. Your objective is to make it out alive. You are playing on Skeld with Blue, Red, and Cyan. \
+However there is an imposter among you who is trying to kill all of you. There are a total of 9 endings, in which 6 are bad, while 3 are good. \
+Will you survive? or will you be a victim to the imposter? Play to find out!'
+
+
+
+ENDING_1 = [
+    'You report the body and the following conversation ensues:',
+
+    '''You: I saw Red kill in admin
+Red: Na fam, it wasn't me
+You: I swear to god it was him, I was going to electrical while I heard a kill sound and when I went there, Red was standing beside the body
+* You vote Red *
+* Red votes you *
+Blue: I mean, Red, I last saw you in admin with Cyan and you were also faking the card swipe even though we have wires as our common task
+Red: Ughhhh fine, I admit its me, BUT Blue do you remember that nude I sent you?
+Blue: *blushing* Yes I do senpai
+Red: I am gonna show you everything in person right now if you vote Yellow
+Blue: Okay! Sounds like a good enough deal to me UwU
+*Blue votes you*''',
+
+    'You realise you lost, its over. The dread spreads across your face as you gets ejected from the crewship. You take one last look at the crewship \
+and you see Red and Blue making out, and you close your eyes for one last time as a tear falls out of your eye while you dissolve into nothingness',
+
+    '''\
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+You got ending #1 - Getting voted out
+Do you want to play again or exit?'''
+]
+
+
+
+ENDING_2 = [
+    'Seeing the imposter with the dead body, you panic and start running. But as you are running...........',
+
+    'You feel a sudden pain in your back, like somebody stabbed you. You feel the blood oozing from your back as you slowly fall down and lose your consciousness',
+
+    '''\
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+You got ending #2 - Getting stabbed
+Do you want to play again or exit?'''
+]
+
+
+
+ENDING_3 = [
+    'You vote Blue and so does Red, and Blue gets ejected out. Its now only Red and you left. Due to your makeout session before, Red doesnt kill you',
+
+    'You marry Red and he impregnates you and live happily ever after with the love of your life',
+
+    '''\
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+You got ending #3 - Marrying red (aka the best ending)
+Do you want to play again or exit?'''
+]
+
+
+
+ENDING_4 = [
+    'You vote Red and get him ejected. The look on his face when he realised you used him, crushed you from inside but you knew what you did was for the best',
+
+    'Although you win the game, as time passes, you start feeling worse and worse. The lonliness comes creeping back and the guilt that you killed one of \
+only people who cared for you. This continues until the pain becomes too much for you to handle and you hang yourself in an attempt to reunite and see \
+your one true love once again',
+    
+    '''\
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+You got ending #4 - Killing yourself to reunite with Red
+Do you want to play again or exit?'''
+]
+
+
+
+ENDING_5 = [
+    'As you are running to electrical, you see Blue pass by you in storage coming from electrical and 10 seconds later.......', 
+
+    'Blue reports Cyan dead body, and the following conversation ensues:',
+
+    '''You: Where?
+Blue: Admin, I saw Yellow coming from there
+Yellow: But I came from cafetaria and I didn't even go into Admin ;-;
+Red: Thats heavy cap, I left Yellow with Cyan in Admin and Yellow was also faking the admin swipe even though we have wires as our common task
+Blue: That's right, I think we all know who the imposter is here
+*Blue votes you*
+*Red votes you*
+You: B-Bu ''',
+
+    'Before you know it, the voting time is over and you are being ejected out of the crewship',
+
+    'You are numb, all you can feel is disbelief, on how easy it was to get you ejected out. You take one last look at the ship and you see blue panicking and red \
+smiling menacingly. This is the last thing you see before finally closing your eyes and embracing your fate',
+
+    '''\
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+You got ending #5 - Getting voted out
+Do you want to play again or exit?'''
+]
+
+
+
+ENDING_6 = [
+    'You vote Red and you watch him as he gets ejected from the ship. It feels like someone has slowed down time, and Red was......',
+
+    'the Imposter! You win and are appreciated by your friends for making the right decision :)',
+
+    '''\
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+You got ending #6 - Getting Red voted out
+Do you want to play again or exit?'''
+]
+
+
+
+ENDING_7 = [
+    'You vote Blue and you watch him as he gets ejected from the ship. It feels like someone has slowed down time, and Blue was......',
+
+    'not the imposter! You suddenly feel an immense sense of dread and you know its over, you lost, you fucked up',
+
+    'You look red in the eye as he takes out his 6 inch knife and he stabs you in the abdomen',
+
+    'You feel the blood leaking out of you as you slowly lose conciousness and your soul dissolves into nothingness',
+
+    '''\
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+    You got ending #7 - Getting stabbed cause you voted wrong
+    Do you want to play again or exit?'''
+]
+
+
+
+ENDING_8 = [
+    'You close your eyes, and relax yourself as your soul dissolves into nothingness',
+
+    '''
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+You got ending #8 - Getting your soul eaten
+Do you want to play again or exit?'''
+]
+
+
+
+ENDING_9 = [
+    'You grab the pill, but it doesn\'t do anything, and you just slip into nothingness. But.......',
+
+    'you feel, not bad. Is this what death feels like? Even the pain is vanishing now. It hits you, that you aren\'t dead. You open your eyes and what you see \
+shocks you. Its........',
+
+    'DA BABY!!',
+
+    'Somehow, the pill summoned Da Baby to help you',
+
+    'Suddenly, Da Baby turns into a convertible!',
+
+    'He says his signature "Less go" before finally vanishing away to save another crewmate from the Jerma Sus monster',
+
+    '''
+███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+You got ending #9 - Getting saved by Da Baby
+Do you want to play again or exit?'''
+]
+
+
+
+
+
+
+
+
+
+
+'''
+Script:
+
+Electrical or medbay?               (n1)
+  |_ Electrical                     (n2)
+  |   |_ Investigate                (n3)
+  |   |   |_ Report end(1)          (n4)
+  |   |   |_ Run end(2)             (n5)
+  |   |   |_ Convince               (n6)
+  |   |       |_ Voteblue end(3)    (n7)
+  |   |       |_ Votered end(4)     (n8)
+  |   |_ Ignore end(5)              (n9)
+  |_ Medbay                         (n10)
+      |_ Red end(6)                 (n11)
+      |_ Blue end(7)                (n12)
+      |_ Skip                       (n13)
+        |_ Giveup end(8)            (n14)
+        |_ Grabpill end(9)          (n15)
+
+
+'''
+
+
+
+
+
+
